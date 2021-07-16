4d5eab242dbec00f13fbbfc4744294419f0f9f6b
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-16 16:32:31 +0200

    Create python package structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="template-loader",
+    version="0.0.2",
+    author="Ruben Lipperts",
+    author_email="",
+    description="Load json (and other) files and replace template values",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rlipperts/template",
+    packages=setuptools.find_packages(include='template_loader'),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+    ],
+    python_requires='~=3.9',
+)
