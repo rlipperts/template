1206a09504025cc83aebffe30edacb899eef4e39
rlipperts <rlipperts@techfak.uni-bielefeld.de>
2021-07-27 11:17:51 +0200

    Implement replacement functionality

--- src/template_loader/loader.py
+++ src/template_loader/loader.py
@@ -36,16 +36,35 @@ def load(path: Path, placeholder_marker_left: str = '${', placeholder_marker_rig
 def build_pattern(placeholder_marker_left: str, placeholder_marker_right: str) -> re.Pattern[str]:
     left = re.escape(placeholder_marker_left)
     right = re.escape(placeholder_marker_right)
-    return re.compile(r'(' + left + r')(.+)(' + right + r')')
+    # regex matches everything between left and right marker, optionally in between quotes
+    # group 'prefix_quote': optional leading quote
+    # group 'placeholder': everythin in between left and right marker (smallest possible match only)
+    # group 'prefix_quote': optional leading quote
+    # see https://regex101.com/r/zEwq7N/1
+    return re.compile(r'(?P<prefix_quote>"?)' + left + r'(?P<placeholder>.+?)' +
+                      right + r'(?P<suffix_quote>"?)')
 
 
 def func_replacer(match: re.Match, safe: bool,
                   **replacements: typing.Union[str, bool, int, float]) -> str:
-    placeholder = match.group(2)
+    prefix_quote = match.group('prefix_quote')
+    placeholder = match.group('placeholder')
+    suffix_quote = match.group('suffix_quote')
     try:
-        return str(replacements[placeholder])
+        # replace correctly so strings are not broken but non-strings are not converted to strings
+        if prefix_quote and not suffix_quote:
+            return prefix_quote + str(replacements[placeholder])
+        elif suffix_quote and not prefix_quote:
+            return str(replacements[placeholder]) + suffix_quote
+        elif prefix_quote and suffix_quote:
+            if not isinstance(replacements[placeholder], str):
+                return str(replacements[placeholder])
+            else:
+                return prefix_quote + str(replacements[placeholder]) + suffix_quote
+        else:
+            return str(replacements[placeholder])
     except KeyError:
         if safe:
-            return str(match)
+            return match.group(0)
         else:
             raise KeyError(f'Missing replacement for placeholder {placeholder}!')
