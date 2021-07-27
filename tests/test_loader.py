c502f3611663392058e3213157100efab83eec3a
rlipperts <rlipperts@techfak.uni-bielefeld.de>
2021-07-27 11:17:31 +0200

    Add tests

---
+++ tests/test_loader.py
@@ -0,0 +1,75 @@
+import json
+import re
+from pathlib import Path
+
+import pytest
+
+from template_loader import loader
+
+
+@pytest.mark.parametrize('placeholder, prefix, suffix',
+                         [('test', '${', '}'),
+                          ('testy_test testy-test-test', '${', '}'),
+                          ('test', '<<', '>>'),
+                          ('testy test testy-test-test', '<<', '>>'), ])
+def test_pattern_creation(placeholder: str, prefix: str, suffix: str):
+    pattern = loader.build_pattern(prefix, suffix)
+    text_fragment = ' Lorem ipsum dolor sit amet, consectetur adipiscing ' \
+                    'elit, sed do eiusmod tempor incididunt ut labore et ' \
+                    'dolore magna aliqua. '
+    a = (text_fragment + placeholder + text_fragment) * 2
+    b = text_fragment + placeholder + ' ' + placeholder + text_fragment
+    matching = prefix + placeholder + suffix
+    text = a + matching + b + matching + b
+    result = re.sub(pattern, '', text)
+    assert result == a + b + b
+
+
+@pytest.mark.parametrize('text',
+                         ['Lorem ${ipsum} dolor sit amet, consectetur adipiscing',
+                          'Lorem ${ipsum} dolor sit amet, consectetur ${adipiscing}',
+                          '${Lorem} ipsum dolor sit amet, consectetur ${adipiscing}', ])
+def test_func_replacer(text: str):
+    replacements = {
+        'Lorem': 'Lorem',
+        'ipsum': 'ipsum',
+        'adipiscing': 'adipiscing',
+    }
+    clean_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing'
+    def replacer(match): return loader.func_replacer(match, False, **replacements)
+    assert re.sub(loader.build_pattern('${', '}'), replacer, text) == clean_text
+
+
+def test_func_replacer_safe_ignores_missing_replacements():
+    replacements = {
+        'ipsum': 'ipsum',
+    }
+    text = 'Lorem ${ipsum} dolor sit amet, consectetur ${adipiscing}'
+    clean_text = 'Lorem ipsum dolor sit amet, consectetur ${adipiscing}'
+    def replacer(match): return loader.func_replacer(match, True, **replacements)
+    assert re.sub(loader.build_pattern('${', '}'), replacer, text) == clean_text
+
+
+def test_func_replacer_unsafe_errors_if_missing_replacements():
+    replacements = {
+        'ipsum': 'ipsum',
+    }
+    text = 'Lorem ${ipsum} dolor sit amet, consectetur ${adipiscing}'
+
+    def replacer(match): return loader.func_replacer(match, False, **replacements)
+    with pytest.raises(KeyError):
+        re.sub(loader.build_pattern('${', '}'), replacer, text)
+
+
+@pytest.mark.parametrize('file_ending', ['json', 'yaml', 'toml'])
+def test_template_load(file_ending):
+    result_path = Path.cwd() / 'data/replaced_placeholders.json'
+    with open(result_path) as file:
+        correct_data = json.load(file)
+    template_path = Path.cwd() / ('data/example_template.' + file_ending)
+    replacements = {
+        'placeholder': 'replacement',
+        'float_placeholder': 3.14159,
+    }
+    check_me = loader.load(template_path, **replacements)
+    assert check_me == correct_data
